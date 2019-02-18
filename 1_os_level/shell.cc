066dc64d564166fb09cc9cf54707fe3dcaabf3c4
Brian van der Bijl <peikos@peikos.net>
2019-02-18 14:58:05 +0100

    Voorbeeld system calls in src functie

--- 1_os_level/shell.cc
+++ 1_os_level/shell.cc
@@ -6,6 +6,7 @@ int main()
   { std::getline(std::cin, input);
     if (input == "new_file") new_file();
     if (input == "ls") list();
+    if (input == "src") src();
     if (input == "find") find();
     if (input == "python") python();
     if (input == "quit") return 0;
@@ -22,3 +23,9 @@ void find()
 
 void python()
 { std::cout << "PYTHON" << std::endl; }
+
+void src()
+{ int fd = syscall(SYS_open, "shell.cc", O_RDONLY, 0755); // Gebruik de SYS_open call om een bestand te openen.
+  char byte[1];                                           // 0755 zorgt dat het bestand de juiste rechten krijgt (leesbaar is).
+  while(syscall(SYS_read, fd, byte, 1))                   // Blijf SYS_read herhalen tot het bestand geheel gelezen is,
+    std::cout << byte; }                                  //   zet de gelezen byte in "byte" zodat deze geschreven kan worden.
--- 1_os_level/shell.hh
+++ 1_os_level/shell.hh
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <unistd.h>  // syscall()
+#include <syscall.h> // SysCall nummers
+#include <fcntl.h>   // O_RDONLY
 
 int main();
 void new_file();
 void list();
 void find();
 void python();
+void src();
