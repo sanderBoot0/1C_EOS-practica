3b889ea04f7d621cbbce253233c44cf9f3b3b20f
sanderBoot0 <43379323+sanderBoot0@users.noreply.github.com>
2019-02-19 10:12:49 +0100

    Update shell.cc

--- 1_os_level/shell.cc
+++ 1_os_level/shell.cc
@@ -1,31 +1,74 @@
 #include "shell.hh"
-
-int main()
-{ std::string input;
-  while(true)
-  { std::getline(std::cin, input);
-    if (input == "new_file") new_file();
-    if (input == "ls") list();
-    if (input == "src") src();
-    if (input == "find") find();
-    if (input == "python") python();
-    if (input == "quit") return 0;
-    if (input == "error") return 1; } }
-
-void new_file()
-{ std::cout << "NEW" << std::endl; }
-
-void list()
-{ std::cout << "LS" << std::endl; }
-
-void find()
-{ std::cout << "FIND" << std::endl; }
-
-void python()
-{ std::cout << "PYTHON" << std::endl; }
-
-void src()
-{ int fd = syscall(SYS_open, "shell.cc", O_RDONLY, 0755); // Gebruik de SYS_open call om een bestand te openen.
-  char byte[1];                                           // 0755 zorgt dat het bestand de juiste rechten krijgt (leesbaar is).
-  while(syscall(SYS_read, fd, byte, 1))                   // Blijf SYS_read herhalen tot het bestand geheel gelezen is,
-    std::cout << byte; }                                  //   zet de gelezen byte in "byte" zodat deze geschreven kan worden.
+ 
+using namespace std;
+ 
+void new_file() {
+    cout << "Give name: ";
+    string fileName;
+    cin >> fileName;
+    ofstream outfile (fileName);
+    string input;
+    while (input != "EOF") {
+        cout << "Give a sentence: ";
+        cin >> input;
+        outfile << input << endl;
+    }
+    outfile.close();
+}
+ 
+void list() {
+    int pid = fork();
+    if (pid == 0) {
+        // char *args[] = {"/bin/ls", "-r", "-t", "-l", (char *) 0};
+        // execv("/bin/ls", args);
+        system("ls -la");
+        _exit(0);
+    } else {
+        int exit_status;
+        wait(&exit_status);
+    }
+}
+ 
+void find() {
+    cout << "Search for: ";
+    string input;
+    cin >> input;
+ 
+    int pid = fork();
+    if (pid == 0) {
+        system("find .");// >> system("grep " + input);
+        _exit(0);
+    } else {
+        int exit_status;
+        wait(&exit_status);
+    }
+}
+ 
+void python() {
+    int pid = fork();
+    if (pid == 0) {
+        system("python");
+        _exit(0);
+    } else {
+        int exit_status;
+        wait(&exit_status);
+    }
+}
+ 
+int main() {
+    string promptLine;
+    ifstream promptFile ("prompt.txt");
+    getline(promptFile, promptLine);
+ 
+    string input;
+    while (true) {
+        cout << promptLine << " ";
+        getline(cin, input);
+        if (input == "new_file") new_file();
+        if (input == "ls") list();
+        if (input == "find") find();
+        if (input == "python") python();
+        if (input == "quit") return 0;
+        if (input == "error") return 1;
+    }
+}
